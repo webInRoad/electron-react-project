2287273a426b920c5376e3ca1d56748c5569ef2e
webInRoad <1040233545@qq.com>
2021-10-07 20:36:14 +0800

    引入 bootstrap 与 styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css'
+import React from 'react'
+import styled from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 自定义左侧容器
+let LeftDiv = styled.div.attrs({
+	className: 'col-3 left-panel'
+})`
+	background-color: #285b41;
+	min-height: 100vh;
+`
+// 自定义右侧容器
+let RightDiv = styled.div.attrs({
+	className: 'col-9 right-panel'
+})`
+	background-color: #8b8e86;
+`
 
 function App() {
 	return (
-		<div className="App">
-			<h2>electron react project</h2>
+		<div className="App container-fluid">
+			<div className="row no-gutters">
+				<LeftDiv>左侧</LeftDiv>
+				<RightDiv>右侧</RightDiv>
+			</div>
 		</div>
 	)
 }
